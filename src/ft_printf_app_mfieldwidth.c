fac7d1470ea260085a056f76dc876eb3d5544a7f
oliver Belanger <obelange@e1z2r9p45.42.us.org>
2016-12-26 23:14:23 -0800

    ft_printf_app_mfieldwidth normed

--- src/ft_printf_app_mfieldwidth.c
+++ src/ft_printf_app_mfieldwidth.c
@@ -19,31 +19,23 @@ void	ft_printf_app_mfieldwidth(t_mod *conv)
 	intmax_t	len;
 	size_t		i;
 
-	// printf("conv->cmfw: %c\n", conv->cmfw);
-	// printf("conv->mfw: %jd\n", conv->mfw);
-	// printf("conv->mfieldwidth: %s\n", conv->mfieldwidth);
-	// printf("0substring: -->%s\n", conv->substring);	
 	if (conv->mfieldwidth)
-	{		
-		// printf("1substring: -->%s\n", conv->substring);
+	{
 		if (ft_printf_stric(conv->conversion, "sSdDioOuUxXcCb%", &i) == 1)
-		{	
-			// printf("2substring: -->%s\n", conv->substring);
+		{
 			len = ft_printf_strlen(conv->substring);
-			index = -1;								
-			ft_printf_strdel(&(conv->mfieldwidth));			
+			index = -1;
+			ft_printf_strdel(&(conv->mfieldwidth));
 			if ((conv->mfw - len) > 0)
-			{	
-				// printf("3substring: -->%s\n", conv->substring);
+			{
 				while (++index < (conv->mfw - len))
-					conv->mfieldwidth = ft_printf_fstrappend(conv->mfieldwidth, conv->cmfw);					
-				tmp = conv->substring;						
-				conv->substring = ft_printf_strinsert(conv->substring, conv->mfieldwidth, 0, 0);			
+					conv->mfieldwidth =
+					ft_printf_fstrappend(conv->mfieldwidth, conv->cmfw);
+				tmp = conv->substring;
+				conv->substring =
+				ft_printf_strinsert(conv->substring, conv->mfieldwidth, 0, 0);
 				ft_printf_strdel(&tmp);
-				// printf("4substring: -->%s\n", conv->substring);
 			}
-			// printf("5substring: -->%s\n", conv->substring);
 		}
-		// printf("6substring: -->%s\n", conv->substring);
 	}
 }
