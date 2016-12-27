1393e0b0c73bff28d298ad1b5ab08754ff246d25
oliver Belanger <obelange@e1z2r9p45.42.us.org>
2016-12-27 00:56:12 -0800

    ft_printf_format_parsing normed

--- src/ft_printf_format_parsing.c
+++ src/ft_printf_format_parsing.c
@@ -13,22 +13,18 @@
 #include "libftprintf.h"
 
 void	ft_printf_format_parsing(char const *format, t_mod *conv, va_list args)
-{	
+{
 	char	*fsub;
 	char	*rem;
 
 	if (conv)
-	{		
+	{
 		ft_printf_stric(format, g_sequence_symbols, &(conv->srt_seq));
-		ft_printf_stric(&((format)[conv->srt_seq + 1]), g_conversion_symbols, &(conv->end_seq));
-		fsub = ft_printf_strsub(format, conv->srt_seq + 1, conv->end_seq + 1);
-		conv->end_seq += conv->srt_seq + 1;		
+		ft_printf_stric(&((format)[conv->srt_seq + 1]),
+			g_conversion_symbols, &(conv->end_seq));
+		fsub = ft_printf_strsub(format, conv->srt_seq + 1,
+			conv->end_seq + 1);
+		conv->end_seq += conv->srt_seq + 1;
 		ft_printf_flow(&fsub, conv, args);
-
-
-		// printf("conv->cmfw: %c\n", conv->cmfw);
-		// printf("conv->mfw: %jd\n", conv->mfw);
-		// printf("conv->mfieldwidth: %s\n", conv->mfieldwidth);
-
 	}
 }
