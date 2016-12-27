2c4ccce35b0db9e9941f7d75805c4f138433a33a
oliver Belanger <obelange@e1z2r9p45.42.us.org>
2016-12-27 00:09:43 -0800

    ft_printf_chng_int normed

--- src/ft_printf_chng_int.c
+++ src/ft_printf_chng_int.c
@@ -13,13 +13,13 @@
 #include "libftprintf.h"
 
 void	ft_printf_chng_int(t_mod *conv, int num)
-{	
+{
 	intmax_t	nam;
 
 	if (num < 0)
 	{
 		nam = num;
-		conv->substring = ft_printf_itoa_base(-nam, 10, 1);	
+		conv->substring = ft_printf_itoa_base(-nam, 10, 1);
 	}
 	else
 		conv->substring = ft_printf_itoa_base(num, 10, 0);
