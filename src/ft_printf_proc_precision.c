a123be141d6c94614302d2787e276cec2d9600e4
oliver Belanger <obelange@e1z2r9p45.42.us.org>
2016-12-27 03:43:04 -0800

    ft_printf_proc_precision normed

--- src/ft_printf_proc_precision.c
+++ src/ft_printf_proc_precision.c
@@ -17,33 +17,24 @@ void	ft_printf_proc_precision(t_mod *conv)
 	size_t	num;
 
 	if (conv->precision)
-	{		
+	{
 		if (ft_printf_stric(conv->conversion, "diouDOU", &num) == 1)
-		{	
-			// printf("1conv->precision: %s\n", conv->precision);
-			ft_printf_proc_setchar(&(conv->precision), &(conv->prec), &(conv->cprec), &(conv->arg_list), '0');			
-		}
+			ft_printf_proc_setchar(&(conv->precision), &(conv->prec),
+									&(conv->cprec), &(conv->arg_list), '0');
 		else if (ft_printf_strchri(conv->conversion, 'x', &num) == 1)
-		{
-			// printf("2conv->precision: %s\n", conv->precision);
-			ft_printf_proc_setchar(&(conv->precision), &(conv->prec), &(conv->cprec), &(conv->arg_list), '0');
-		}
+			ft_printf_proc_setchar(&(conv->precision), &(conv->prec),
+									&(conv->cprec), &(conv->arg_list), '0');
 		else if (ft_printf_strchri(conv->conversion, 'X', &num) == 1)
-			{
-				// printf("3conv->precision: %s\n", conv->precision);
-			ft_printf_proc_setchar(&(conv->precision), &(conv->prec), &(conv->cprec), &(conv->arg_list), '0');
-			}
+			ft_printf_proc_setchar(&(conv->precision), &(conv->prec),
+									&(conv->cprec), &(conv->arg_list), '0');
 		else if (ft_printf_stric(conv->conversion, "sS", &num) == 1)
-			{
-				// printf("4conv->precision: %s\n", conv->precision);
-			ft_printf_proc_setchar(&(conv->precision), &(conv->prec), &(conv->cprec), &(conv->arg_list), '\0');
-			}
+			ft_printf_proc_setchar(&(conv->precision), &(conv->prec),
+									&(conv->cprec), &(conv->arg_list), '\0');
 		else
 		{
-			// printf("5conv->precision: %s\n", conv->precision);
-			ft_printf_proc_setchar(&(conv->precision), &(conv->prec), &(conv->cprec), &(conv->arg_list), '\0');
+			ft_printf_proc_setchar(&(conv->precision), &(conv->prec),
+									&(conv->cprec), &(conv->arg_list), '\0');
 			ft_printf_strdel(&(conv->precision));
 		}
-
 	}
 }
