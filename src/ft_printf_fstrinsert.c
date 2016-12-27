6b3511e6c30174083b98bbb795b6fa3e3c1a2501
oliver Belanger <obelange@e1z2r9p45.42.us.org>
2016-12-27 01:19:54 -0800

    ft_printf_fstrinsert normed

--- src/ft_printf_fstrinsert.c
+++ src/ft_printf_fstrinsert.c
@@ -12,22 +12,17 @@
 
 #include "libftprintf.h"
 
-void	ft_printf_fstrinsert(char **printit, char *substr, size_t strt, size_t end)
+void	ft_printf_fstrinsert(char **printit, char *substr,
+								size_t strt, size_t end)
 {
 	char	*tmp;
 	char	*str;
-		
+
 	str = *printit;
-	tmp = ft_printf_fstrmcat(ft_printf_strsub(str, 0, strt), substr);	
-	// printf("tmp: %s\n", tmp);
-	// printf("(str + end): %s\n", (str + end));
-	// printf("strt: %d\n", strt);
-	// printf("end: %d\n", end);	
-	if (*tmp == '\0' && *(str + end) == '\0')		
-		*printit = NULL;	
+	tmp = ft_printf_fstrmcat(ft_printf_strsub(str, 0, strt), substr);
+	if (*tmp == '\0' && *(str + end) == '\0')
+		*printit = NULL;
 	else
-		*printit = ft_printf_fstrmcat(tmp, (str + end));	
-	
-	// printf("*printit: %s\n", *printit);
+		*printit = ft_printf_fstrmcat(tmp, (str + end));
 	ft_printf_strdel(&str);
 }
