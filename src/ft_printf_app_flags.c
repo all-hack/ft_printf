fce1b1526c662b70394104bdb82855ce2226d198
oliver Belanger <obelange@e1z2r9p45.42.us.org>
2016-12-26 23:11:26 -0800

    ft_printf_app_flags normed

--- src/ft_printf_app_flags.c
+++ src/ft_printf_app_flags.c
@@ -14,31 +14,22 @@
 
 void	ft_printf_app_flags(t_mod *conv)
 {
-	int	i;
+	int		i;
 	char	*flags;
 	size_t	j;
 
-	// printf("conv->prec: %d\n", conv->prec);
-	if (conv->prec != 0 || conv->num != 0 )
+	if (conv->prec != 0 || conv->num != 0)
 	{
-		// if (ft_printf_stric(conv->conversion, "CcsS%", &j))
-		// {
-		// printf("conv->num: %d\n", conv->num);
-		// printf("conv->flag: %s\n", conv->flag);
-			if (conv->flag && !ft_printf_stric(conv->conversion, "p", &j))
+		if (conv->flag && !ft_printf_stric(conv->conversion, "p", &j))
+		{
+			i = 0;
+			flags = "#0- +";
+			while (*g_printf_flags[i] != 0)
 			{
-				// printf("conv->flag: %s\n", conv->flag);
-				i = 0;
-				flags = "#0- +";
-				while (*g_printf_flags[i] != 0)
-				{		
-					// printf("%s\n", flags);
-					// printf("%c\n", flags[i]);			
-					if (ft_printf_strchri(conv->flag, flags[i], &j))
-						(*g_printf_flags[i]) (conv);
-					i++;
-				}
+				if (ft_printf_strchri(conv->flag, flags[i], &j))
+					(*g_printf_flags[i])(conv);
+				i++;
 			}
-		}	
-	// }
+		}
+	}
 }
