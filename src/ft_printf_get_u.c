7faa074f5f2279a7e7db13ecbe0676e449df7ba0
oliver Belanger <obelange@e1z2r9p45.42.us.org>
2016-12-27 02:31:24 -0800

    ft_printf_get_u normed

--- src/ft_printf_get_u.c
+++ src/ft_printf_get_u.c
@@ -13,19 +13,19 @@
 #include "libftprintf.h"
 
 void	ft_printf_get_u(t_mod *conv, va_list args)
-{		
+{
 	if (!(conv->length))
-		ft_printf_chng_uint(conv, va_arg(args, unsigned int));	
+		ft_printf_chng_uint(conv, va_arg(args, unsigned int));
 	else if (ft_printf_strcmp(conv->length, "h") == 0)
 		ft_printf_chng_ushort(conv, va_arg(args, unsigned int));
 	else if (ft_printf_strcmp(conv->length, "j") == 0)
 		ft_printf_chng_uintmax_t(conv, va_arg(args, uintmax_t));
-	else if (ft_printf_strcmp(conv->length, "l") == 0)		
+	else if (ft_printf_strcmp(conv->length, "l") == 0)
 		ft_printf_chng_ulong(conv, va_arg(args, unsigned long));
-	else if (ft_printf_strcmp(conv->length, "z") == 0)		
+	else if (ft_printf_strcmp(conv->length, "z") == 0)
 		ft_printf_chng_size_t(conv, va_arg(args, size_t));
 	else if (ft_printf_strcmp(conv->length, "hh") == 0)
 		ft_printf_chng_uchar(conv, va_arg(args, unsigned int));
-	else if (ft_printf_strcmp(conv->length, "ll") == 0)		
+	else if (ft_printf_strcmp(conv->length, "ll") == 0)
 		ft_printf_chng_ulonglong(conv, va_arg(args, unsigned long long));
 }
