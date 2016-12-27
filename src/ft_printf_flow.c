199e8e0d5993f16732b9a8679800d65b1583502b
oliver Belanger <obelange@e1z2r9p45.42.us.org>
2016-12-27 00:54:53 -0800

    ft_printf_flow normed

--- src/ft_printf_flow.c
+++ src/ft_printf_flow.c
@@ -12,46 +12,21 @@
 
 #include "libftprintf.h"
 
-void ft_printf_flow(char **seq, t_mod *conv, va_list args)
+void	ft_printf_flow(char **seq, t_mod *conv, va_list args)
 {
 	int	i;
 
-	// printf("1 parse\n");
-	// printf("conv->substring: %s\n", conv->substring);
-	// printf("conv->mfieldwidth: %s\n", conv->mfieldwidth);
-	// printf("conv->precision: %s\n", conv->precision);
-	// printf("conv->flag: %s\n", conv->flag);
 	i = 0;
 	while (*g_printf_parse[i] != 0)
-		(*g_printf_parse[i++]) (seq, conv);	
-	// printf("[detection]  conv->flag: %s\n", conv->flag);
-	// printf("2 process\n");
-	// printf("conv->substring: %s\n", conv->substring);
-	// printf("conv->mfieldwidth: %s\n", conv->mfieldwidth);
-	// printf("conv->precision: %s\n", conv->precision);
-	// printf("conv->flag: %s\n", conv->flag);
-	i = 0;	
+		(*g_printf_parse[i++])(seq, conv);
+	i = 0;
 	while (*g_printf_process[i] != 0)
 	{
-		// printf("i: %d\n", i);
-		(*g_printf_process[i++]) (conv);
+		(*g_printf_process[i++])(conv);
 	}
-	// printf("[processing] conv->flag: %s\n", conv->flag);
-	// printf("3 apply\n");
-	// printf("conv->substring: %s\n", conv->substring);
-	// printf("conv->mfieldwidth: %s\n", conv->mfieldwidth);
-	// printf("conv->precision: %s\n", conv->precision);
-	// printf("conv->flag: %s\n", conv->flag);
 	i = 0;
-	while (*g_printf_apply[i] != 0)	
+	while (*g_printf_apply[i] != 0)
 	{
-		// printf("i: %d\n", i);
-		// printf("conv->substring: %s\n", conv->substring);
-		(*g_printf_apply[i++]) (conv);		
+		(*g_printf_apply[i++])(conv);
 	}
-	// printf("4\n");
-	// printf("conv->substring: %s\n", conv->substring);
-	// printf("conv->mfieldwidth: %s\n", conv->mfieldwidth);
-	// printf("conv->precision: %s\n", conv->precision);
-	// printf("conv->flag: %s\n", conv->flag);
 }
