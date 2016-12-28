f7a2956cadaae1d890742a491d30628b08a693a9
oliver Belanger <obelange@e1z2r8p4.42.us.org>
2016-12-28 01:05:50 -0800

    ft_printf_symdet_conv normed

--- src/ft_printf_symdet_conv.c
+++ src/ft_printf_symdet_conv.c
@@ -13,18 +13,19 @@
 #include "libftprintf.h"
 
 void	ft_printf_symdet_conv(char **seq, t_mod *conv)
-{	
-	size_t	delim_index;	
+{
+	size_t	delim_index;
 	size_t	i;
 
 	i = 0;
-	delim_index = 0;	
-	if (ft_printf_1chr_nsym(*seq, g_conversion_symbols, &(conv->conversion)) == 1)
-	{		
+	delim_index = 0;
+	if (ft_printf_1chr_nsym(*seq, g_conversion_symbols,
+													&(conv->conversion)) == 1)
+	{
 		while (i < ft_printf_strlen(conv->conversion))
 		{
 			(*seq)++;
 			i++;
-		}	
+		}
 	}
 }
