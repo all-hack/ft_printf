f5ca8c4fa3b36ea0e32f02aeead27a464ed1c870
oliver Belanger <obelange@e1z2r8p4.42.us.org>
2016-12-28 00:58:17 -0800

    ft_printf_strinsert normed

--- src/ft_printf_strinsert.c
+++ src/ft_printf_strinsert.c
@@ -12,15 +12,16 @@
 
 #include "libftprintf.h"
 
-char	*ft_printf_strinsert(char *printit, char *substr, size_t strt, size_t end)
+char	*ft_printf_strinsert(char *printit, char *substr,
+													size_t strt, size_t end)
 {
 	char	*tmp;
 	char	*str;
 
 	tmp = ft_printf_fstrmcat(ft_printf_strsub(printit, 0, strt), substr);
-	if (*tmp || *(printit + end))		
+	if (*tmp || *(printit + end))
 		str = ft_printf_fstrmcat(tmp, (printit + end));
 	else
-		str = NULL;		
-	return(str);
+		str = NULL;
+	return (str);
 }
